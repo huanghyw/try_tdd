c78a4daca6e5fccd1fd3afe06695da0375c7c058
HuangYiwei <huanghyw@gmail.com>
2020-11-03 18:16:18 +0800

    根据根路径解析到页面单元测试编写完成，但是抛出解析404异常

--- lists/tests.py
+++ lists/tests.py
@@ -1,7 +1,10 @@
 from django.test import TestCase
+from django.urls import resolve
+from lists.views import home_page
 
 
-class SmokeTest(TestCase):
+class HomePageTest(TestCase):
 
-    def test_bad_maths(self):
-        self.assertEqual(True, False)
+    def test_root_url_resolves_to_home_page(self):
+        found = resolve("/")
+        self.assertEqual(found.func, home_page)
