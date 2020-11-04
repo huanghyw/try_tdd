d08d8078d5c596324794c8ae7ae585d4047c9227
HuangYiwei <huanghyw@gmail.com>
2020-11-04 15:16:43 +0800

    解决功能测试抛出的异常

--- lists/views.py
+++ lists/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 
 def home_page(request):
-    return HttpResponse("<html><title>To-Do lists</title></html>")
+    return HttpResponse("<html><title>待办事项【To-Do lists】</title></html>")
