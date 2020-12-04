6b3196ca8e7e6c2af7bb45ed9bcd7c78b421bb58
HuangYiwei <huanghyw@gmail.com>
2020-12-04 11:00:18 +0800

    提交待办事项后跳转到首页，并且可以显示所有已添加的待办事项

--- lists/views.py
+++ lists/views.py
@@ -1,8 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+
+from lists.models import Item
 
 
 def home_page(request):
-    return render(request, 'home.html', {
-        'new_item_text': request.POST.get('item_text'),
-    })
+    if 'POST' == request.method:
+        new_item_text = request.POST.get('item_text', '')
+        Item.objects.create(text=new_item_text)
+        return redirect('/')
+
+    return render(request, 'home.html', {'items': Item.objects.all()})
